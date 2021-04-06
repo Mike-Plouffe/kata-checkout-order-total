b37938ced0502e7eecc7f9d0398473064293eeff
MikePlouffe <61197366+Mike-Plouffe@users.noreply.github.com>
2021-04-06 11:46:28 -0400

    Test for storemarkdowns

--- tests/store.test.js
+++ tests/store.test.js
@@ -1,8 +1,8 @@
 import Store from "../src/Store.js";
 
-describe("the cost of", () => {
-    const store = new Store();
+const store = new Store();
 
+describe("the cost of", () => {
     test("one can of soup is $1.89", () => {
         expect(store.itemsList.soup).toBeCloseTo(1.89);
     });
@@ -13,5 +13,17 @@ describe("the cost of", () => {
 
     test("one lb of bananas is $2.38", () => {
         expect(store.itemsList.banana).toBeCloseTo(2.38);
+    });
+});
+
+describe("the cost with markdowns", () => {
+    test("one can soup during a $.20 markdown period", () => {
+        store.markdown("soup", .2);
+        expect(store.itemsList.soup).toBeCloseTo(.2);
+    });
+
+    test("one can soup after the markdowns are over", () => {
+        store.weeklyReset()
+        expect(store.itemsList.soup).toBeCloseTo(1.89);
     });
 });
