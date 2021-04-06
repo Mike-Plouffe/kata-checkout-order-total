4e1422adef9b52d4a518f727ab1fc7e85f0671a8
MikePlouffe <61197366+Mike-Plouffe@users.noreply.github.com>
2021-04-06 12:51:12 -0400

    Add scanning/removing from cart

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,3 +1,38 @@
-class Checkout {}
+import Store from "./Store.js";
+
+class Checkout {
+    constructor() {
+        this.store = new Store();
+        this.newCart();
+    }
+
+    newCart() {
+        this.cart = {};
+    }
+
+    scan(item, units = 1) {
+        const current = this.cart[item];
+        if (current) {
+            this.cart[item] = current + units;
+        } else {
+            this.cart[item] = units;
+        }
+    }
+
+    remove(item, units = 1) {
+        const current = this.cart[item];
+        let newunits = 0;
+        if (!this.store.itemsByWeight.has(item)) {
+            newunits = current - Math.floor(units);
+        } else {
+            newunits = current - units;
+        }
+        if (newunits > 0) {
+            this.cart[item] = newunits;
+        } else {
+            delete this.cart[item];
+        }
+    }
+}
 
 export default Checkout;
--- src/Store.js
+++ src/Store.js
@@ -1,18 +1,23 @@
 class Store {
     constructor() {
-        this.itemsList = {};
-
         this.weeklyReset();
     }
 
     weeklyReset() {
+        this.itemsList = {};
+        this.itemsByWeight = new Set();
         this.setItem("soup", 1.89);
-        this.setItem("beef", 5.99);
-        this.setItem("banana", 2.38);
+        this.setItem("beef", 5.99, true);
+        this.setItem("banana", 2.38, true);
     }
 
-    setItem(item, price) {
+    setItem(item, price, byWeight = false) {
         this.itemsList[item] = price;
+        if (byWeight) {
+            this.itemsByWeight.add(item);
+        } else {
+            this.itemsByWeight.delete(item);
+        }
     }
 }
 export default Store;
