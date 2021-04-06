56f6aac60d70291f1b59d9ba856e1219fef61c4f
MikePlouffe <61197366+Mike-Plouffe@users.noreply.github.com>
2021-04-06 09:54:53 -0400

    Initial commit
    - Sets up directory structure
    - Defines eslint specs
    - Defines prettier formatting specs

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * Checkout Order Total Kata
+ * MIT Licensed
+ */
+
+import Checkout from "./src/Checkout.js";
+
+export default Checkout;
---
+++ src/Checkout.js
@@ -0,0 +1,4 @@
+class Checkout {
+}
+
+export default Checkout;
