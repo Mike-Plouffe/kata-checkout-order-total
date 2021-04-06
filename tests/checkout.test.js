667f1cfe9983b44e57c041c720856563fe64c803
MikePlouffe <61197366+Mike-Plouffe@users.noreply.github.com>
2021-04-06 12:17:01 -0400

    Test for Scanning and Removing from cart

---
+++ tests/checkout.test.js
@@ -0,0 +1,30 @@
+import Checkout from "../src/Checkout.js";
+
+const checkout = new Checkout()
+
+describe('Scanning:', () => {
+    test('the cart initially has no items', () => {
+        expect(Object.keys(checkout.cart).length).toBeCloseTo(0)
+    })
+    test('1.5lb of beef can be scanned into the cart', () => {
+        checkout.scan('beef', 1.5)
+        expect(Object.keys(checkout.cart).length).toBeCloseTo(1)
+        expect(checkout.cart.beef).toBeCloseTo(1.5)
+    })
+    test('2 cans of soup can be scanned into the cart', () => {
+        checkout.scan('soup')
+        checkout.scan('soup')
+        expect(Object.keys(checkout.cart).length).toBeCloseTo(2)
+        expect(checkout.cart.soup).toBeCloseTo(2)
+    })
+    test('1 can of soup can be removed from the cart', () => {
+        checkout.remove('soup')
+        expect(Object.keys(checkout.cart).length).toBe(2)
+        expect(checkout.cart.soup).toBeCloseTo(1)
+    })
+    test('.5 lb of beef can be removed from the cart', () => {
+        checkout.remove('beef', .5)
+        expect(Object.keys(checkout.cart).length).toBe(2)
+        expect(checkout.cart.beef).toBeCloseTo(1)
+    })
+})
